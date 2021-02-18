d7a4d1533c8937878810c96c69c800ab8625527b
Mahmoud-Barry <mahmoudbarrysn@gmail.com>
2021-02-18 11:47:36 +0000

    final commit

--- js/anniversaire.js
+++ js/anniversaire.js
@@ -1,27 +1,39 @@
 
-var $anniversaire = $('.anniversaire');
+/*selectionner tous les paragraphe */
 var $allText = $('.anniversaire p');
-$allText.hide();
+$allText.hide(); /*masquer les paragraphes */
 
+/*recuperer le premier element */
 var $firstItem = $('.special-day');
 
 $firstItem.on('click', function(e){
     e.preventDefault();
     var $span = $('.special-day span');
-    $span.remove();
-    $next = $firstItem.next();
-    $next.show();
+    $span.remove(); 
+    $allText.first().show();
 });
 
-var $item =$('.annif-text');
-$item.on('click',function(e){
+/*identifier les paragraphes*/
+var $first  = $('.annif-text:eq(0)');
+var $second = $('.annif-text:eq(1)');
+var $last   = $('.annif-text:eq(2)');
+
+/* les evenements */
+$first.on('click',function(e){
     e.preventDefault();
-    $next = $item.after();
-    $next.show();
+    $second.show();
 });
 
+$second.on('click',function(e){
+    e.preventDefault();
+    $last.show();
+});
 
-
+$last.on('click', function(e){
+    $allText.text("Happy birthday !") ;
+    var $bg = $('.bg-info');
+    $bg.css("background-image", "url('img/bg.jpg')"); 
+});
 
 
 
